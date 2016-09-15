b3ac1be1e0c171c4a8da275667fe7003c01eaef4
Aerath Aiskurimu <aerath@users.noreply.github.com>
2016-09-15 19:58:53 +0200

    Use headers from source rather than toolchain ones

--- config.mk
+++ config.mk
@@ -13,7 +13,8 @@ OPTIMIZATION_LEVEL = 0
 FLAGS = -fdiagnostics-color=auto -O$(OPTIMIZATION_LEVEL)
 FLAGS += -Wall -Wextra -Wpointer-arith -Winit-self -Wcast-align -Wfloat-equal -Wsystem-headers -Wbad-function-cast -Wcast-align -Wmissing-format-attribute -Wnested-externs -Wdisabled-optimization -Winline -Wunreachable-code -Wswitch-enum
 FLAGS += -Werror=implicit -Werror=redundant-decls -Werror=ignored-qualifiers -Werror=double-promotion -Werror=nonnull -Werror=shadow -Werror=missing-braces -Werror=return-type
+FLAGS += -nostdinc -I/usr/lib/gcc/$(TARGET)/$(shell $(CC) -v 2>&1|grep version|cut -d' ' -f3)/include
 DEBUGFLAGS = -DDEBUG -g
 
-CFLAGS = $(FLAGS) -std=gnu99 $(DEBUGFLAGS)
+CFLAGS = $(DEBUGFLAGS) -std=gnu99 $(FLAGS)
 ASFLAGS = -c -Iinclude $(DEBUGFLAGS)
