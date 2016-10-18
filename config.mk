44fc26f28d370bf304f4a1ad04be7442a1faa107
Aerath Aiskurimu <aerath@users.noreply.github.com>
2016-10-18 22:21:21 +0200

    Added support for c++ in build scripts

--- config.mk
+++ config.mk
@@ -2,6 +2,7 @@
 
 TARGET = i386-aeros
 CC = $(TARGET)-gcc
+CXX = $(TARGET)-g++
 LD = $(TARGET)-ld
 AR = $(TARGET)-ar
 AS = $(TARGET)-gcc
@@ -10,11 +11,16 @@ STRIP = $(TARGET)-strip
 MAKEFLAGS =
 
 OPTIMIZATION_LEVEL = 0
-FLAGS = -fdiagnostics-color=auto -O$(OPTIMIZATION_LEVEL) -ftree-vectorize
-FLAGS += -Wall -Wextra -Wpointer-arith -Winit-self -Wcast-align -Wfloat-equal -Wsystem-headers -Wbad-function-cast -Wcast-align -Wmissing-format-attribute -Wnested-externs -Wdisabled-optimization -Winline -Wunreachable-code -Wswitch-enum
-FLAGS += -Werror=implicit -Werror=redundant-decls -Werror=ignored-qualifiers -Werror=double-promotion -Werror=nonnull -Werror=shadow -Werror=missing-braces -Werror=return-type -Werror=incompatible-pointer-types
-FLAGS += -nostdinc -I/usr/lib/gcc/$(TARGET)/$(shell $(CC) -v 2>&1|grep version|cut -d' ' -f3)/include
+
+CPPFLAGS = -fdiagnostics-color=auto -O$(OPTIMIZATION_LEVEL) -ftree-vectorize
+CPPFLAGS += -Wall -Wextra -Wpointer-arith -Winit-self -Wcast-align -Wfloat-equal -Wsystem-headers -Wcast-align -Wmissing-format-attribute -Wdisabled-optimization -Winline -Wunreachable-code -Wswitch-enum
+CFLAGS = -Wbad-function-cast -Wnested-externs
+CPPFLAGS += -Werror=implicit -Werror=redundant-decls -Werror=ignored-qualifiers -Werror=double-promotion -Werror=nonnull -Werror=shadow -Werror=missing-braces -Werror=return-type -Werror=incompatible-pointer-types
+CPPFLAGS += -nostdinc -I/usr/lib/gcc/$(TARGET)/$(shell $(CC) -v 2>&1|grep version|cut -d' ' -f3)/include
+CFLAGS += -std=gnu99
+
 DEBUGFLAGS = -DDEBUG -g
 
-CFLAGS = $(DEBUGFLAGS) -std=gnu99 $(FLAGS)
+CPPFLAGS += $(DEBUGFLAGS)
+
 ASFLAGS = -c -Iinclude $(DEBUGFLAGS)
